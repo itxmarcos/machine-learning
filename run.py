a1123da333e3ed6fe9e80ba019595d73568bf691
Marcos Caballero <mcabally@gmail.com>
2022-11-12 13:49:24 +0100

    Initial commit

---
+++ run.py
@@ -0,0 +1,3 @@
+# run.py
+
+import test3
---
+++ test.py
@@ -0,0 +1,15 @@
+# https://realpython.com/python-logging/
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s::: %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+str = 'wawawiwa'
+logging.debug(f'My string is {str}')
+
+################################
+a = 5
+b = 0
+try:
+  c = a / b
+except Exception as e:
+  logging.exception("Exception occurred", exc_info=True)
